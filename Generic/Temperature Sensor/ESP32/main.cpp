5cb9f98f00d2e138b37f4ff03c3c13a6db5f84c2
The Geek Explorer <mark@markgreenall.uk>
2021-07-14 12:39:40 +0100

    Update main.cpp

--- Generic/Temperature Sensor/ESP32/main.cpp
+++ Generic/Temperature Sensor/ESP32/main.cpp
@@ -16,28 +16,32 @@ WebServer server(80);
 String header;
 
 
+/**
+ * Setup method for ESP32
+ * @param void
+ * @return void
+ */
 void setup() {
 
-  // Setup SERIAL
+  // SERIAL - Setup serial
   Serial.begin(9600);
   Serial.println("Serial Started.");
   delay(1000);
   
-  // Setup WIFI
+  // WIFI - Setup wifi
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
+  WiFi.disconnect(); delay(100);
   WiFi.setHostname(HOSTNAME);
   WiFi.begin(SSIDNAME, SSIDPASS);
   WiFi.setHostname(HOSTNAME);
   
-  // Wait until connected
+  // WIFI - Wait until connected
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
     delay(100);
   }
 
-  // Connected
+  // WIFI - Display connection details
   Serial.println("Connected.");
   Serial.print("Device IP: ");
   Serial.println(WiFi.localIP());
@@ -49,15 +53,18 @@ void setup() {
   Serial.println(WiFi.RSSI());
   delay(1000);
   
-  // Setup web server
+  // WEB SERVER - Define Routes
   server.on("/",                HTTP_GET,  handleRoute_root);
   server.on("/dashboard/",      HTTP_POST, handleRoute_dashboard);
   server.on("/api/temperature", HTTP_GET,  handleRoute_temperature);
   server.on("/api/pressure",    HTTP_GET,  handleRoute_pressure);
+
+  // WEB SERVER - Start Server!
   server.begin();
   Serial.println("API Server Started.");
   delay(1000);
-  
+
+  // BMP180 - Test component
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085/BMP180 sensor, check wiring!");
     while (1) {}
@@ -65,20 +72,22 @@ void setup() {
 }
 
 
-
+/**
+ * Main loop method
+ * @param void
+ * @return void
+ */
 void loop() {
   server.handleClient();
 }
 
 
-
 /**
  * ROUTE - "/"
+ * @param void
+ * @return void
  */
 void handleRoute_root() {
-  setHeaders_NoCache();
-  setHeaders_CrossOrigin();
-  
   char msg[2000];
   strcpy(msg, "<html>");
   strcat(msg, "  <head>");
@@ -107,18 +116,21 @@ void handleRoute_root() {
   strcat(msg, "    </main>");
   strcat(msg, "  </body>");
   strcat(msg, "</html>");
-  
+
+  // Send content to client
+  setHeaders_NoCache();
+  setHeaders_CrossOrigin();
   server.send(200, "text/html", msg);
 }
 
 
 /**
  * ROUTE - "/dashboard/"
+ * @param void
+ * @return void
  */
 void handleRoute_dashboard() {
-  setHeaders_NoCache();
-  setHeaders_CrossOrigin();
-
+  
   // Check login credentials
   String username = server.arg("username");
   String password = server.arg("password");
@@ -149,94 +161,100 @@ void handleRoute_dashboard() {
   strcat(msg, "        <a href='/api/pressure'>/api/pressure</a><br>");
   strcat(msg, "      </p>");
   strcat(msg, "      <p>");
-  strcat(msg, "        <a href='/'>[X] Logout</a><br>");
+  strcat(msg, "        <a href='/'>[x] Logout</a><br>");
   strcat(msg, "      </p>");
   strcat(msg, "    </main>");
   strcat(msg, "  </body>");
   strcat(msg, "</html>");
-  
+
+  // Send content to client
+  setHeaders_NoCache();
+  setHeaders_CrossOrigin();
   server.send(200, "text/html", msg);
 }
 
 
-
 /**
  * ROUTE - "/api/temperature"
+ * @param void
+ * @return void
  */
 void handleRoute_temperature() {
-  setHeaders_NoCache();
-  setHeaders_CrossOrigin();
-  
   float r = 0.00;
   char r1[10];
-  int count = 0;
-  char count1[10];
   char msg[1000];
   
-  // read sensor
-  for (int i=0; i<40; i++) {
-    count = i;
-    r = bmp.readTemperature();
-    if (r > -20 && r < 40)
-      break;
-    delay(50);
-  }
-
-  // format the response
+  // Read sensor
+  r = readSensor("temperature");
   sprintf (r1, "%f", r);
-  sprintf (count1, "%i", count);
-  
+
+  // Build content
   strcpy(msg, "{result:{\"ROUTE\":\"temperature\",\"status\":\"OK\", \"value\":\"");
   strcat(msg, r1);
-  strcat(msg, "\", ");
-  strcat(msg, "\"count\":");
-  strcat(msg, count1);
   strcat(msg, "\"}}");
 
-  // Send to client
+  // Send content to client
+  setHeaders_NoCache();
+  setHeaders_CrossOrigin();
   server.send(200, "application/json", msg);
 }
-
-
 
 
 /**
  * ROUTE - "/api/pressure"
+ * @param void
+ * @return void
  */
 void handleRoute_pressure() {
-  setHeaders_NoCache();
-  setHeaders_CrossOrigin();
-  
   float r = 0.00;
   char r1[10];
-  int count = 0;
-  char count1[2];
   char msg[1000];
   
-  // read sensor
-  for (int i=0; i<40; i++) {
-    count = i;
-    r = bmp.readPressure();
-    if (r > 850 && r < 120000)
-      break;
-    delay(50);
-  }
-
-  // format the response
+  // Read sensor
+  r = readSensor("pressure");
   sprintf (r1, "%f", r);
-  sprintf (count1, "%i", count);
-  
+
+  // Build content
   strcpy(msg, "{result:{\"ROUTE\":\"pressure\",\"status\":\"OK\", \"value\":\"");
   strcat(msg, r1);
-  strcat(msg, "\", ");
-  strcat(msg, "\"count\":");
-  strcat(msg, count1);
   strcat(msg, "\"}}");
 
-  // Send to client
+  // Send content to client
+  setHeaders_NoCache();
+  setHeaders_CrossOrigin();
   server.send(200, "application/json", msg);
 }
 
+
+/**
+ * Reas the sensor and return the value
+ * @param String sensor
+ * @param float r
+ * @return char*
+ */
+float readSensor (String sensor) {
+  float r = 0.00;
+  char r1[10];
+  
+  // read sensor
+  for (int i=0; i<40; i++) {
+
+    // TEMPERATURE SENSOR
+    if (sensor == "Temperature") {
+      r = bmp.readTemperature();
+      if (r > -20 && r < 40)
+        break;
+        
+    // PRESSURE SENSOR
+    } else if (sensor == "Pressure") {
+      r = bmp.readPressure();
+      if (r > 850 && r < 120000)
+        break;
+    }
+    delay(50);
+  }
+  return r;
+}
 
 void setHeaders_NoCache(){
   server.sendHeader(F("Expires"),       F("-1"));
@@ -250,3 +268,4 @@ void setHeaders_CrossOrigin(){
   server.sendHeader(F("Access-Control-Allow-Methods"), F("PUT,POST,GET,OPTIONS"));
   server.sendHeader(F("Access-Control-Allow-Headers"), F("*"));
 };
+
